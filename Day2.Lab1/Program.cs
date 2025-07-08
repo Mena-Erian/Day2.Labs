f80762d3093552759771cdd9beb2799f4415a824
Mena Erian Farouk <135561286+Mena-Erian@users.noreply.github.com>
2025-07-08 16:30:07 +0300

    Add project files.

---
+++ Day2.Lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Day1.Lab2;
+using System;
+
+namespace Day1.Lab1
+{
+    class Program
+    {
+        static void Main()
+        {
+            #region Point
+            //point p1 = new point() { x = 50, y = 75, z = 90 };
+            //point p2 = new point() { x = 70, y = 65, z = 80 };
+
+            //p1.displayAll();
+
+            //p2.displayAll();
+
+            //Console.WriteLine($"the result of calculation distance = {p2.CalcDistance(p1)} "); 
+            #endregion
+
+
+            #region Fraction
+            Fraction f1 = new Fraction() { nom = 5, Den = 6 };
+            Fraction f2 = new Fraction() { nom = 2, Den = 5 };
+
+            f1.display();
+
+            f2.display();
+
+            Fraction res = f1.add(f1, f2);
+            Console.WriteLine($"The Sum = {res.nom} / {res.Den} "); 
+            #endregion
+
+
+
+        }
+    }
+}
